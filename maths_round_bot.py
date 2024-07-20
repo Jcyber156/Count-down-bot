a517b087e15ee1fa1e1517b5bb3e24bf9d500be4
Jcyber156 <cyberman.organisim@gmail.com>
2024-07-21 00:51:34 +0100

    Focussing on trying different strategies instead of brute forcing (prime factors)

--- maths_round_bot.py
+++ maths_round_bot.py
@@ -1,4 +1,5 @@
 import itertools
+import Create_prime_factor_tree
 
 #Maths round bot
 
@@ -9,41 +10,19 @@ import itertools
 #Get as close as possible to the target number 
 
 class MathsBot:
+    #Idea 1 :split into products of primes. If it is a prime number, find the primes surrounding the number
     def __init__(self,target,num_list) -> None:
         self.target = target 
         self.num_list = num_list #0-5 index
         self.operations = ["+","-","*","/"]
 
-    def generate_all_possible_combos(self) -> list:
-        #generates all possible permutations of operationsof length 1-r (power set)
-        combined_list = self.operations + self.num_list #concatinate lists (combine)
-        self.generated_poweset = self.powerset(combined_list)
+        #make prime factor tree
+        self.prime_factor_tree = Create_prime_factor_tree.Tree(root_value= self.target)
+        self.prime_factor_tree.make_tree()
 
 
 
-    def powerset(self,iterable):
-        
-        
-        '''"powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-        s = list(iterable)
-        return list(itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(3,len(s)+1)))'''
-
-
-    def evaluate_powerset(self):
-        #1) get all legitimate combinations
-        #2) evaluate them and find the closest to the target 
-        closest_answer = 0 
-        
-        
-        for phrase in self.generated_poweset:
-            for expression in list(itertools.permutations(phrase)):
-                current_ans = self.make_and_eval_rpn(expression)
-                if current_ans is not False and abs(self.target-current_ans) < abs(self.target- closest_answer): #if legit and closer
-                    closest_answer = current_ans
-        return closest_answer
-
-
-    def make_and_eval_rpn(self,expression):
+    def eval_rpn(self,expression):
         stack = []
         ops_performed = False
         for item in expression:
@@ -75,16 +54,12 @@ class MathsBot:
             return stack[0] #final answer
         return False
 
-    def run(self):
-        self.generate_all_possible_combos()
-        answer = self.evaluate_powerset()
-        print(f"Closest answer: {answer}")
 
 
 if __name__ == "__main__":
+    num_list  = []
+    num_list+= str(input("Enter numbers (seperated by a space): ")).split(" ")
     target = int(input("What is the target? "))
-    num_list = []
-    num_string = input("What are the numbers (seperate with a space)? ")
-    num_list += num_string.split(" ")
     maths_bot = MathsBot(target,num_list)
-    maths_bot.run()
+
+    print(maths_bot.prime_factor_tree.collect_prime_factors())
